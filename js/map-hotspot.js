ac6c109a42176dc9bbe75fc0f0bfb0d1d8aaafb0
Muhamad Diaz <muhamaddiaz10@gmail.com>
2025-06-20 09:22:05 +0700

    fix(map): click legend to trigger card

--- js/map-hotspot.js
+++ js/map-hotspot.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     const hotspots = document.querySelectorAll('.map-hotspot');
+    const legendHotspots = document.querySelectorAll('.map-legend-hotspot');
     const infoCards = document.querySelectorAll('.map-info-card');
     const closeButtons = document.querySelectorAll('.close-info-card');
 
@@ -7,8 +8,32 @@ window.addEventListener('DOMContentLoaded', () => {
         infoCards.forEach(card => card.classList.remove('active'));
     };
 
+    const showInfoCard = (location) => {
+        closeAllInfoCards();
+
+        const infoCard = document.getElementById(`${location}-card`);
+    
+        if (infoCard) {
+            infoCard.classList.add('active');
+            
+            const mapHotspot = document.querySelector(`[data-location="${location}"]`);
+            if (mapHotspot) {
+                const hotspotRect = mapHotspot.getBoundingClientRect();
+                const mapRect = document.querySelector('.map-image').getBoundingClientRect();
+
+                const top = hotspotRect.top - mapRect.top;
+                const left = hotspotRect.left - mapRect.left;
+
+                infoCard.style.top = `${top - 20}px`;
+                infoCard.style.left = `${left + 70}px`;
+            }
+        }
+    };
+
     document.addEventListener('click', e => {
-        if (!e.target.closest('.map-hotspot') && !e.target.closest('.map-info-card')) {
+        if (!e.target.closest('.map-hotspot') && 
+            !e.target.closest('.map-info-card') && 
+            !e.target.closest('.map-legend-hotspot')) {
             closeAllInfoCards();
         }
     });
@@ -21,21 +46,15 @@ window.addEventListener('DOMContentLoaded', () => {
         hotspot.addEventListener('click', e => {
             e.stopPropagation();
             const location = hotspot.dataset.location;
+            showInfoCard(location);
+        });
+    });
 
-            closeAllInfoCards();
-            const infoCard = document.getElementById(`${location}-card`);
-
-            if (infoCard) {
-                infoCard.classList.add('active');
-
-                const hotspotRect = hotspot.getBoundingClientRect();
-                const mapRect = document.querySelector('.map-image').getBoundingClientRect();
-                const top = hotspotRect.top - mapRect.top;
-                const left = hotspotRect.left - mapRect.left;
-
-                infoCard.style.top = `${top - 20}px`;
-                infoCard.style.left = `${left + 70}px`;
-            }
+    legendHotspots.forEach(legendHotspot => {
+        legendHotspot.addEventListener('click', e => {
+            e.stopPropagation();
+            const location = legendHotspot.dataset.location;
+            showInfoCard(location);
         });
     });
 }); 
